c6a85f2a4d43bfc217d10dc803886e3219cb6b9a
VictorAzox <victor_hugo_rei@hotmail.com>
2024-02-26 18:18:07 -0400

    up

--- pythonProject/Alura_curso/app.py
+++ pythonProject/Alura_curso/app.py
@@ -1,4 +1,7 @@
-print (""" 
+import os
+
+def exiber_nome_programa():
+    print (""" 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -7,10 +10,36 @@ print ("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
  """)
 
-print ('1. Cadastrar Restaurante')
-print ('2. Listar Restaurante')
-print ('3. Ativar Restaurante')
-print ('4. Sair')
+def exibir_opcoes():
+    print ('1. Cadastrar Restaurante')
+    print ('2. Listar Restaurante')
+    print ('3. Ativar Restaurante')
+    print ('4. Sair')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print (f'Você escolheu a opção : {opcao_escolhida}')
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+
+    if opcao_escolhida == 1 :
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2 :
+        print('Listar Restaurante')
+    elif opcao_escolhida == 3 :
+        print('Ativar Restaurante')
+    else :
+        finalizar_app()
+
+
+
+def main():
+    exiber_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+    
+
+if __name__ == '__main__':
+    main()
