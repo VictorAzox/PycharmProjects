31cea16d051d6d20294e5566ec051c8c943ea932
VictorAzox <victor_hugo_rei@hotmail.com>
2024-02-27 18:03:46 -0400

    update

--- pythonProject/Alura_curso/app.py
+++ pythonProject/Alura_curso/app.py
@@ -1,5 +1,8 @@
 import os
 
+restaurantes = ['Pobres Lanches', 'Lanches Ricos']
+
+
 def exiber_nome_programa():
     print (""" 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -17,25 +20,57 @@ def exibir_opcoes():
     print ('4. Sair')
 
 def finalizar_app():
+    exibir_subtitulo('Finalizando o app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal ')
+    main()
+
+def opcao_invalida():
+    print('Opção invalida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
     os.system('cls')
-    print('Finalizando o app\n')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de Novos Restaurantes')
+  
+    nome_restaurante = input("Digite o nome do Restaurante de que desaja cadastrar: ")
+    restaurantes.append(nome_restaurante)
+    print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso! ')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listar Todos Restaurantes')
+    for restaurante in restaurantes :
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
 
-    if opcao_escolhida == 1 :
-        print('Cadastrar Restaurante')
-    elif opcao_escolhida == 2 :
-        print('Listar Restaurante')
-    elif opcao_escolhida == 3 :
-        print('Ativar Restaurante')
-    else :
-        finalizar_app()
-
+        if opcao_escolhida == 1 :
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2 :
+            listar_restaurantes()
+        elif opcao_escolhida == 3 :
+            print('Ativar Restaurante')
+        elif opcao_escolhida == 4 :
+            finalizar_app()
+        else :
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 
 def main():
+    os.system('cls')
     exiber_nome_programa()
     exibir_opcoes()
     escolher_opcao()
