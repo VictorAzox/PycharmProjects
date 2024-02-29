1eb334d9086697e4ea1c7effd733ed9c605f35e4
Victor <victor_hugo_rei@hotmail.com>
2024-02-29 12:26:07 -0400

    up

--- pythonProject/Alura_curso/app.py
+++ pythonProject/Alura_curso/app.py
@@ -42,7 +42,10 @@ def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de Novos Restaurantes')
   
     nome_restaurante = input("Digite o nome do Restaurante de que desaja cadastrar: ")
-    restaurantes.append(nome_restaurante)
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_restaurante}: ')
+    dados_do_restaurante = {'nome':nome_restaurante,'categoria':categoria, 'ativo':False}
+
+    restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso! ')
     voltar_ao_menu_principal()
 
@@ -56,6 +59,23 @@ def listar_restaurantes():
 
     voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -66,7 +86,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2 :
             listar_restaurantes()
         elif opcao_escolhida == 3 :
-            print('Ativar Restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4 :
             finalizar_app()
         else :
